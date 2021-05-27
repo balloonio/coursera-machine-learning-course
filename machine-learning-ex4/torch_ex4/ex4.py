ad13b7e3f1e7b99958c15805c92e981180bdbe85
Bolun Zhang <rzhangbolun@gmail.com>
2021-05-27 02:50:02 -0400

    Added plt to visulize training data image

--- machine-learning-ex4/torch_ex4/ex4.py
+++ machine-learning-ex4/torch_ex4/ex4.py
@@ -1,6 +1,7 @@
 import scipy.io as spio
 from typing import List
 import torch
+import matplotlib.pyplot as plt
 
 def read_mat(file_path:str, keys: List[str]):
     mat_data = spio.loadmat(file_path)
@@ -11,13 +12,29 @@ def read_mat(file_path:str, keys: List[str]):
         result.append(data)
     return result
 
+def show_training_images(X, h, w):
+    m = X.shape[0]
+
+    def show_training_image(x):
+        reshaped_x = torch.reshape(x, (h,w))
+        reshaped_x = torch.rot90(reshaped_x, 3, [0, 1]) # not sure why its 90 degree off
+        reshaped_x = torch.fliplr(reshaped_x) # not sure why flipped
+        plt.imshow(reshaped_x)
+        plt.show()
+
+    for _ in range(min(m,10)):
+        from random import randint
+        i = randint(0, m-1)
+        show_training_image(X[i])
+
+
 
 def main():
     training_file = './ex4data1.mat'
     X, y = read_mat(training_file, ['X', 'y'])
     # print(X)
     # print(y)
-    print(X.shape)
-
+    # print(X.shape)
+    show_training_images(X, 20, 20)
 
 main()
